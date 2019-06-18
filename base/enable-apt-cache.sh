8f8893391f897f81119010c783c24fad83f2ca11
linuxaddict89 <ekoputrapratama27@yahoo.com>
2019-06-19 03:58:45 +0800

    update

--- base/enable-apt-cache.sh
+++ base/enable-apt-cache.sh
@@ -4,8 +4,9 @@
 if [ -f /etc/apt/apt.conf.d/docker-clean ]; then
   rm /etc/apt/apt.conf.d/docker-clean
 fi
-# change apt cache directory
+
 if [ ! -f /etc/apt/apt.conf.d/docker-apt-cache ]; then
+  # change apt cache directory
   echo "Dir{Cache \"/tmp/apt/\";};" >> /etc/apt/apt.conf.d/docker-apt-cache
   echo "APT{NeverAutoRemove {\".deb$\";}};" >> /etc/apt/apt.conf.d/docker-apt-cache
 fi
